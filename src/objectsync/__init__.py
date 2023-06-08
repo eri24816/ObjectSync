c31d363bda3feba00eaa2879e44ac2621f234499
eri24816 <a931eric@gmail.com>
2023-06-08 17:42:47 +0800

    Remove unused import

--- src/objectsync/__init__.py
+++ src/objectsync/__init__.py
@@ -2,6 +2,3 @@ from .server import Server
 from .sobject import SObject
 from chatroom.topic import Topic, IntTopic, SetTopic, DictTopic, StringTopic, ListTopic, GenericTopic, FloatTopic
 from objectsync.topic import ObjListTopic, ObjSetTopic, ObjDictTopic, ObjTopic
-
-import chatroom
-#chatroom.set_level(3)
