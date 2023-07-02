7b1dc37b186f193dae69a5b059ce6f23a787e5dd
eri24816 <a931eric@gmail.com>
2023-07-02 21:35:15 +0800

    Export EventTopic

--- src/objectsync/__init__.py
+++ src/objectsync/__init__.py
@@ -1,4 +1,4 @@
 from .server import Server
 from .sobject import SObject
-from chatroom.topic import Topic, IntTopic, SetTopic, DictTopic, StringTopic, ListTopic, GenericTopic, FloatTopic
+from chatroom.topic import Topic, IntTopic, SetTopic, DictTopic, StringTopic, ListTopic, GenericTopic, FloatTopic, EventTopic
 from objectsync.topic import ObjListTopic, ObjSetTopic, ObjDictTopic, ObjTopic
